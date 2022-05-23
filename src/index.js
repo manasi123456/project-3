81549ff4ad3bcfecb14b84692242603412d2db40
Manasi Joshi <manasi04joshi@gmail.com>
2022-05-23 16:38:44 +0530

    aws..

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://lddu818:27o3D6VwW2z1zHMj@cluster0.6gomf.mongodb.net/group11Database",
-//     { useNewUrlParser: true }
-//   )
-//   .then(() => console.log("MongoDB is connected"))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(
+    "mongodb+srv://lddu818:27o3D6VwW2z1zHMj@cluster0.6gomf.mongodb.net/group11Database",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
 
 // app.use("/", route);
 
